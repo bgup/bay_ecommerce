e33c04e545c41f8ee175c938add229675e7429e0
byron_dev <bayronuruena@gmail.com>
2023-11-29 18:36:50 -0500

    16-mongoconnection-enviroment
    se utiliza la dependecia dotenv para las variables de entorno y se  accede la cadena de conección por el objeto proceess

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import mongoose from 'mongoose';
 
 const dbConnect = async()=>{
     try {
-        const connected = mongoose.connect('url');
+        const connected = mongoose.connect(process.env.MONGO_URL);
+        mongoose.set("strictQuery",true)
         console.log(`Mongodb connected ${(await connected).connection.host}`);
+
     } catch (error) {
         console.log(`Error ${error.message}`);
         //ver la documantación
