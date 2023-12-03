8609639d83de634f409d0b7b44a94b9449922d7c
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-02 20:32:37 -0500

    19 User registration controller
    Se agrega validación de user existente y post de user

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -5,7 +5,19 @@ import User from '../model/User.js';
 // @acces Private/Admin
 
 export const registerUserCtrl = async (req, res) => {
-    res.json({
-        msg: "User register controller"
+    const { fullname, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.json({
+            msg: "User already exists"
+        })
+    }
+    const user = await User.create({
+        fullname, email, password
+    })
+    res.status(201).json({
+        status: 'success',
+        message: 'User create successfuly',
+        data: user
     })
 };
