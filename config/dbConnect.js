bff4b677ddd4eae8aee6fe215846ffd8a9088455
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-13 20:22:45 -0500

    25 - Mongoose Warning Fixed English

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,14 +3,14 @@ dotenv.config()
 import mongoose from 'mongoose';
 
 
-const dbConnect = async()=>{
+const dbConnect = async () => {
     try {
+        mongoose.set("strictQuery", false)
         const connected = mongoose.connect(process.env.MONGO_URL);
-        mongoose.set("strictQuery",true)
         console.log(`Mongodb connected ${(await connected).connection.host}`);
     } catch (error) {
         console.log(`Error ${error.message}`);
-        
+
         process.exit(1);
     }
 }
