6e00ae946b853087cc2464b396c02a6e1e8dceb4
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-13 20:21:58 -0500

    23 - Not Found Route Handler
    Se configura midleware not found para entregar err a globalErrhandler

--- app/app.js
+++ app/app.js
@@ -3,13 +3,14 @@ dotenv.config();
 import express from 'express';
 import dbConnect from '../config/dbConnect.js';
 import userRoutes from '../routes/userRoutes.js';
-import { globalErrhandler } from '../midlewares/globalErrHandler.js';
+import { globalErrhandler, notFound } from '../midlewares/globalErrHandler.js';
 dbConnect();
 
 const app = express();
 app.use(express.json());
 app.use('/', userRoutes);
 //errr middleware
-app.use(globalErrhandler)
+app.use(notFound);
+app.use(globalErrhandler);
 export default app;
- 
+  
