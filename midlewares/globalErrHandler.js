d3ea014c59db7c0a9b5ea4591e053f400ddcaa84
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-13 20:20:37 -0500

    23 - Not Found Route Handler
    Se crea función para err 404

--- midlewares/globalErrHandler.js
+++ midlewares/globalErrHandler.js
@@ -1,13 +1,19 @@
-export const globalErrhandler=(err,req,res,next ) =>{
-//stack
-//message
-const stack= err?.stack;
-const statusCode = err?.statusCode ? err?.statusCode : 500;
+export const globalErrhandler = (err, req, res, next) => {
+    //stack
+    //message
+    const stack = err?.stack;
+    const statusCode = err?.statusCode ? err?.statusCode : 500;
 
-const message = err?.message;
-res.status(statusCode).json({
-    stack,
-    message,
-});
+    const message = err?.message;
+    res.status(statusCode).json({
+        stack,
+        message,
+    });
 
 };
+
+//404 handler
+export const notFound = (req, res, next) => {
+    const err = new Error(`Route ${req.originalUrl} not found`);
+    next(err);  
+}
