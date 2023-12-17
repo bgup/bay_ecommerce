f2971581ae7795e45899da028190849437af26a0
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-17 07:09:27 -0500

    30 - IsLogin Middleware
    se cambia return a false si hay error en validación de token para manjar en msg de error enel middleware

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = token => {
     return jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
-        if(err)return 'Token invalid/expired';
+        if(err)return false;
         return decoded;
     });
 };
