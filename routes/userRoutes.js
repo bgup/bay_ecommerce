b16f8e15da43b19aa554c955f08a4d8d065815ef
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-17 07:06:27 -0500

    30 - IsLogin Middleware
    se usa middeware isLogged en el las rutas de profile user para validar token antes del controlador

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { registerUserCtrl, loginUserCtrl, getUserProfileCtrl } from '../controllers/userCtrl.js';
-
+import {isLoggedIn} from '../midlewares/isLoggedIn.js'
 const userRoutes = express.Router();
 
 userRoutes.post('/register', registerUserCtrl);
 userRoutes.post('/login', loginUserCtrl);
-userRoutes.get('/profile', getUserProfileCtrl);
+userRoutes.get('/profile',isLoggedIn, getUserProfileCtrl);
 
 
 export default userRoutes;
