11a19dc25047715103a146eb386cbf441ead59db
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-17 06:19:36 -0500

    29 - Verify Generated Token
    se crea función para validar token usando jwt

---
+++ utils/verifyToken.js
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = token => {
+    return jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+        if(err)return 'Token invalid/expired';
+        return decoded;
+    });
+};
