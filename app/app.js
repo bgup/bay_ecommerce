8af0b8f4ec765770817d3902ca7d9247f8f45446
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-17 20:53:40 -0500

    32 - Create Product Controller
    Se configura productRouter

--- app/app.js
+++ app/app.js
@@ -3,12 +3,14 @@ dotenv.config();
 import express from 'express';
 import dbConnect from '../config/dbConnect.js';
 import userRoutes from '../routes/userRoutes.js';
+import productRoutes from '../routes/productsRoutes.js';
 import { globalErrhandler, notFound } from '../midlewares/globalErrHandler.js';
 dbConnect();
 
 const app = express();
 app.use(express.json());
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/products', productRoutes);
 //errr middleware
 app.use(notFound);
 app.use(globalErrhandler);
