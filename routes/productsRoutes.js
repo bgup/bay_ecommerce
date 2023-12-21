1990ae17640a29efc6726cdd2aba8793ba07bf59
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-21 16:54:02 -0500

    40 - Fetching Single Product
    Se crea controlador para obtener un product y se configura en router

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createProductCtrl, getProductsCtrl } from '../controllers/productCtrl.js';
+import { createProductCtrl, getProductsCtrl, getProductCtrl } from '../controllers/productCtrl.js';
 import { isLoggedIn } from '../midlewares/isLoggedIn.js'
 
 
 const productRoutes = express.Router();
 productRoutes.post('/', isLoggedIn, createProductCtrl);
 productRoutes.get('/', getProductsCtrl);
+productRoutes.get('/:id', getProductCtrl);
 
 
 
