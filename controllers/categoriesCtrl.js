fe3031c6e798286d748c1050f6ae174dfe9e4a60
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2023-12-22 14:16:42 -0500

    44 - Single Update Delete Categories Controller

--- controllers/categoriesCtrl.js
+++ controllers/categoriesCtrl.js
@@ -16,10 +16,63 @@ export const createCategoryCtrl = asyncHandler(async (req, res) => {
         user: req.userAuthID
     })
     console.log(category.user);
-    
+
     res.json({
-        status:'success',
+        status: 'success',
         message: 'Category created successfully',
         category
     });
 });
+
+// @desc Get categories
+// @route GET /api/v1/categories
+// @access Public
+export const getAllCategoriesCtlr = asyncHandler(async (req, res) => {
+    const categories = await Category.find();
+    res.json({
+        status: "success",
+        message: 'Categories fetching successfuly',
+        categories
+    });
+});
+
+// @desc Get single category
+// @route GET /api/v1/categories/:id
+// @access Public
+export const getCategoryCtrl = asyncHandler(async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+        throw new Error('Category no found.');
+    }
+    res.json({
+        status: 'success',
+        message: 'Category fetched successfully',
+        category
+    })
+});
+
+// @desc update single category
+// @route PUT /api/v1/categories/:id
+// @access Private/Admin
+export const updateCategoryCtrl = asyncHandler(async (req, res) => {
+    const { name } = req.body;
+    const categoryUpdated = await Category.findByIdAndUpdate(req.params.id, { name }, { new: true });
+    res.json({
+        status: 'success',
+        message: 'Category updated successfully',
+        categoryUpdated
+    });
+});
+
+// @desc delete category
+// @route DELETE /api/v1/categories/:id
+// @access Private/Admin
+export const deleteCategoryCtrl = asyncHandler(async (req, res) => {
+    await Category.findByIdAndDelete(req.params.id);
+    res.json({
+        status: 'success',
+        message: 'Category deleted successfully',
+    })
+});
+
+
--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { createCategoryCtrl } from '../controllers/categoriesCtrl.js';
+import {
+    createCategoryCtrl, getAllCategoriesCtlr, getCategoryCtrl, deleteCategoryCtrl, updateCategoryCtrl
+} from '../controllers/categoriesCtrl.js';
 import { isLoggedIn } from '../midlewares/isLoggedIn.js'
 
 
 const categoryRoutes = express.Router();
 categoryRoutes.post('/', isLoggedIn, createCategoryCtrl);
+categoryRoutes.get('/', getAllCategoriesCtlr);
+categoryRoutes.get('/:id', getCategoryCtrl);
+categoryRoutes.put('/:id', isLoggedIn, updateCategoryCtrl);
+categoryRoutes.delete('/:id/delete', isLoggedIn, deleteCategoryCtrl);
+
 
 
 
