16d33e9531ba081cd1a98c17760ffea5847852ea
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2024-01-22 19:35:50 -0500

    GET all orders route - 69 Fetch All Orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import {
     createOrderCtrl, 
-    // getAllOrdersCtlr, getOrderCtrl, updateOrderCtrl, deleteOrderCtrl
+    getAllOrdersCtrl
+    // getOrderCtrl, updateOrderCtrl, deleteOrderCtrl
 } from '../controllers/orderCtrl.js';
 import { isLoggedIn } from '../midlewares/isLoggedIn.js'
 
 
 const orderRoutes = express.Router();
 orderRoutes.post('/', isLoggedIn, createOrderCtrl);
-// orderRoutes.get('/', getAllOrdersCtlr);
+orderRoutes.get('/', isLoggedIn, getAllOrdersCtrl);
 // orderRoutes.get('/:id', getorderCtrl);
 // orderRoutes.put('/:id', isLoggedIn, updateOrderCtrl);
 // orderRoutes.delete('/:id/delete', isLoggedIn, deleteOrderCtrl);
